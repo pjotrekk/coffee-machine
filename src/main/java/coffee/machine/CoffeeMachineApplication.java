fd34fecdfcdb56ff747bda2616e3d210eef09956
pjotrekk <pi.szuberski1@gmail.com>
2020-02-02 09:33:54 +0100

    Init project with Spring Initializr

---
+++ src/main/java/coffee/machine/CoffeeMachineApplication.java
@@ -0,0 +1,13 @@
+package coffee.machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/coffee/machine/CoffeeMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package coffee.machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
