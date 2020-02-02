70560fb0fd6bc8fe190ab56445a543112edbeb12
pjotrekk <pi.szuberski1@gmail.com>
2020-02-02 19:51:53 +0100

    Remove unnecessary bean

--- src/main/java/coffee/machine/CoffeeMachineApplication.java
+++ src/main/java/coffee/machine/CoffeeMachineApplication.java
@@ -1,12 +1,8 @@
 package coffee.machine;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 @SpringBootApplication
@@ -16,20 +12,6 @@ public class CoffeeMachineApplication {
 		SpringApplication app = new SpringApplication(CoffeeMachineApplication.class);
 		app.setDefaultProperties(Collections.singletonMap("server.port", 8086));
 		app.run(args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-		};
 	}
 
 }
