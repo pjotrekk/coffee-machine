6fadf83a35731c66c11ef813f74b1b1d7197f433
pjotrekk <pi.szuberski1@gmail.com>
2020-02-05 20:41:48 +0100

    Run app on the default 8080 port

--- src/main/java/coffee/machine/CoffeeMachineApplication.java
+++ src/main/java/coffee/machine/CoffeeMachineApplication.java
@@ -3,15 +3,11 @@ package coffee.machine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 @SpringBootApplication
 public class CoffeeMachineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication app = new SpringApplication(CoffeeMachineApplication.class);
-		app.setDefaultProperties(Collections.singletonMap("server.port", 8086));
-		app.run(args);
+		SpringApplication.run(CoffeeMachineApplication.class);
 	}
 
 }
